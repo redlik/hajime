88344ddff5097d116729283928bf99cf36f040d5
redlik <ralphr@outlook.com>
2020-10-23 15:42:02 +0100

    buttton component styles

---
+++ resources/views/clubs/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="sm:container sm:mx-auto sm:mt-10">
+        <div class="w-full sm:px-6">
+
+            <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
+
+                <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
+                    Clubs
+                </header>
+
+                <div class="w-full p-6">
+                    <p class="text-gray-700">
+                        <x-button class="button-success" href="/">
+                            Click me
+                        </x-button>
+                    </p>
+                </div>
+            </section>
+        </div>
+    </main>
+@endsection
