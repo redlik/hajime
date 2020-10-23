bf4c535b3f31e4f010ea9a10cec6e0b4b8f343e9
redlik <ralphr@outlook.com>
2020-10-23 16:21:44 +0100

    Todo added

--- database/migrations/2020_10_23_115633_create_clubs_table.php
+++ database/migrations/2020_10_23_115633_create_clubs_table.php
@@ -14,6 +14,7 @@ class CreateClubsTable extends Migration
     public function up()
     {
         Schema::create('clubs', function (Blueprint $table) {
+            // TODO add all fields to the clubs table
             $table->id();
             $table->string('name');
             $table->timestamps();
