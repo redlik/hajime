3dcf22b3a48db8dd5136b3dd2cc0351eb4b1ca6b
redlik <ralphr@outlook.com>
2020-11-03 08:25:01 +0000

    roles seeder

---
+++ database/seeders/PermissionsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role1 = Role::create(['name' => 'user']);
+        $role2 = Role::create(['name' => 'admin']);
+        $role3 = Role::create(['name' => 'personnel ']);
+    }
+}
