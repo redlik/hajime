3c70852e5f83ef05f9eb1296526e2af07abce497
redlik <ralphr@outlook.com>
2020-11-05 15:38:46 +0000

    town to city replacement

--- resources/views/clubs/edit.blade.php
+++ resources/views/clubs/edit.blade.php
@@ -79,7 +79,7 @@
                                 Town / City
                             </label>
                             <input class="shadow appearance-none border rounded w-1/2 py-2 px-3 text-grey-darker"
-                                id="town" name="town" type="text" value="{{ $club->city }}">
+                                id="city" name="city" type="text" value="{{ $club->city }}">
                         </div>
                         <div class="mb-4">
                             <label class="inline-block w-48 text-grey-darker text-sm font-bold mb-2" for="county">
