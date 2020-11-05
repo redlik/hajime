b78e9f16a1fc4e88ee45313133b33d1a1abf72e8
redlik <ralphr@outlook.com>
2020-11-05 16:26:41 +0000

    favicon included

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    
+
     <title>{{ config('app.name', 'Hajime') }}</title>
 
     <!-- Scripts -->
