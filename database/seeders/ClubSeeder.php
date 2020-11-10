a97549e45786214068bd271b77220855f0663402
redlik <ralphr@outlook.com>
2020-11-10 09:38:09 +0000

    Clubnotes factory and seeder

--- database/seeders/ClubSeeder.php
+++ database/seeders/ClubSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Database\Seeders\ClubSeeder;
 use App\Models\Club;
 
 class ClubSeeder extends Seeder
--- database/seeders/ClubnoteSeeder.php
+++ database/seeders/ClubnoteSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Database\Seeders\ClubnoteSeeder;
 use App\Models\Clubnote;
 
 class ClubnoteSeeder extends Seeder
