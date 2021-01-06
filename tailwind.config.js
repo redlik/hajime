54a9205a7c6e196f4345c51a0d3a9599187a97b1
redlik <ralphr@outlook.com>
2021-01-06 16:14:29 +0000

    Added production purge info to Tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: {
+      content: [
+          './resources/views/**/*.blade.php',
+      ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
