de1f6c943a2013761ca8fc2d6aa12bdcce216ad7
redlik <ralphr@outlook.com>
2023-03-10 16:26:29 +0000

    Clubs view controller setup

---
+++ app/Http/Controllers/ClubViewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClubViewController extends Controller
+{
+    public function index()
+    {
+       return true;
+    }
+}
