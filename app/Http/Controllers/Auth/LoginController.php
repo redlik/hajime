fb71f81f7b4faa680bc30a4563d20546dd6787ab
redlik <ralphr@outlook.com>
2023-03-13 17:05:53 +0000

    Pending account middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+//    protected $redirectTo = RouteServiceProvider::HOME;
+
+
+    public function redirectTo()
+    {
+        if (auth()->user()->hasRole('manager')) {
+            return '/club-access/users';
+        }
+        return '/home';
+    }
 
     /**
      * Create a new controller instance.
