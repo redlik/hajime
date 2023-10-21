3466226d0310b933f645ef520e3653f493ba2a7e
redlik <ralphr@outlook.com>
2023-10-21 09:50:28 +0100

    Vite update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,43 @@
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
 const colors = require('tailwindcss/colors')
 
-module.exports = {
-  purge: {
-      content: [
-          './resources/views/**/*.blade.php',
-      ]
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-        colors: {
-            lightjudo: {
-                '50':  '#fafbf7',
-                '100': '#f6fae3',
-                '200': '#e9f5aa',
-                '300': '#d3ea64',
-                '400': '#97d524',
-                '500': '#56bc0b',
-                '600': '#399d06',
-                '700': '#317c09',
-                '800': '#285c0d',
-                '900': '#1f470e',
+
+export default {
+    content: ['./resources/**/*.blade.php', './vendor/filament/**/*.blade.php'],
+    theme: {
+        extend: {
+            colors: {
+                lightjudo: {
+                    '50':  '#fafbf7',
+                    '100': '#f6fae3',
+                    '200': '#e9f5aa',
+                    '300': '#d3ea64',
+                    '400': '#97d524',
+                    '500': '#56bc0b',
+                    '600': '#399d06',
+                    '700': '#317c09',
+                    '800': '#285c0d',
+                    '900': '#1f470e',
+                },
+                judo: {
+                    '50': '#A4EAA5',
+                    '100': '#8FE591',
+                    '200': '#66DB68',
+                    '300': '#3DD13F',
+                    '400': '#2AB22B',
+                    '500': '#019903',
+                    '600': '#0A850C',
+                    '700': '#065507',
+                    '800': '#032603',
+                    '900': '#000000',
+                },
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
             },
-            judo: {
-                '50': '#A4EAA5',
-                '100': '#8FE591',
-                '200': '#66DB68',
-                '300': '#3DD13F',
-                '400': '#2AB22B',
-                '500': '#019903',
-                '600': '#0A850C',
-                '700': '#065507',
-                '800': '#032603',
-                '900': '#000000',
-            },
-            amber: colors.amber,
-        }
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-      require('@tailwindcss/typography'),
-  ],
+    plugins: [forms, typography],
 }
