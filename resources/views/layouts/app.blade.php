b93e8d4f4f2296b97946fecfa3e053a585570894
redlik <ralphr@outlook.com>
2023-10-22 06:47:19 +0100

    Revert "Vite update"
    This reverts commit 3466226d0310b933f645ef520e3653f493ba2a7e.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,6 @@
 
     <title>{{ config('app.name', 'Hajime') }}</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,47 @@
-import forms from '@tailwindcss/forms'
-import typography from '@tailwindcss/typography'
 const colors = require('tailwindcss/colors')
 
-
-export default {
-    content: ['./resources/**/*.blade.php', './vendor/filament/**/*.blade.php'],
-    theme: {
-        extend: {
-            colors: {
-                lightjudo: {
-                    '50':  '#fafbf7',
-                    '100': '#f6fae3',
-                    '200': '#e9f5aa',
-                    '300': '#d3ea64',
-                    '400': '#97d524',
-                    '500': '#56bc0b',
-                    '600': '#399d06',
-                    '700': '#317c09',
-                    '800': '#285c0d',
-                    '900': '#1f470e',
-                },
-                judo: {
-                    '50': '#A4EAA5',
-                    '100': '#8FE591',
-                    '200': '#66DB68',
-                    '300': '#3DD13F',
-                    '400': '#2AB22B',
-                    '500': '#019903',
-                    '600': '#0A850C',
-                    '700': '#065507',
-                    '800': '#032603',
-                    '900': '#000000',
-                },
-                danger: colors.rose,
-                primary: colors.blue,
-                success: colors.green,
-                warning: colors.yellow,
+module.exports = {
+  purge: {
+      content: [
+          './resources/views/**/*.blade.php',
+      ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        colors: {
+            lightjudo: {
+                '50':  '#fafbf7',
+                '100': '#f6fae3',
+                '200': '#e9f5aa',
+                '300': '#d3ea64',
+                '400': '#97d524',
+                '500': '#56bc0b',
+                '600': '#399d06',
+                '700': '#317c09',
+                '800': '#285c0d',
+                '900': '#1f470e',
             },
-        },
+            judo: {
+                '50': '#A4EAA5',
+                '100': '#8FE591',
+                '200': '#66DB68',
+                '300': '#3DD13F',
+                '400': '#2AB22B',
+                '500': '#019903',
+                '600': '#0A850C',
+                '700': '#065507',
+                '800': '#032603',
+                '900': '#000000',
+            },
+            amber: colors.amber,
+        }
     },
-    plugins: [forms, typography],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+      require('@tailwindcss/typography'),
+  ],
 }
