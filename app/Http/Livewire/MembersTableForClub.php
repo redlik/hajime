8be729bea2d5f9289f6a71c7302cf34e992fbc6f
redlik <ralphr@outlook.com>
2023-10-22 07:01:16 +0100

    members view for club

---
+++ app/Http/Livewire/MembersTableForClub.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Member;
+use Livewire\Component;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Contracts\View\View;
+use Livewire\WithPagination;
+
+class MembersTableForClub extends Component
+{
+    use WithPagination;
+
+    public $club_id;
+    public $searchQuery = '';
+    public $sortby = 'last_name';
+
+    public function mount($club_id)
+    {
+        $this->club_id = $club_id;
+        $this->searchQuery = '';
+    }
+
+    public function updatingSearchquery()
+    {
+        $this->resetPage();
+    }
+
+    public function render()
+    {
+        $members = Member::when($this->searchQuery != '', function ($query) {
+            $query->where('first_name', 'like', '%' . $this->searchQuery . '%')
+                ->orWhere('last_name', 'like', '%' . $this->searchQuery . '%')
+                ->orWhere('number', 'like', '%' . $this->searchQuery . '%')
+                ->orWhere('eircode', 'like', '%' . $this->searchQuery . '%');
+        })->where('club_id', $this->club_id)
+            ->orderBy($this->sortby, 'asc')
+            ->paginate(15);
+
+        return view('livewire.club-members', compact('members'));
+    }
+}
