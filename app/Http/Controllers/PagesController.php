10d59d5e530e0fcd1d0c186f3ccca76cbb710200
ralph-redlich <ralph.redlich@pantheon.io>
2024-04-09 15:56:03 +0100

    Admin list in controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Spatie\Activitylog\Models\Activity;
+use Spatie\Permission\Models\Role;
 
 class PagesController extends Controller
 {
     public function logsPage()
     {
-        $logs = Activity::all();
+        $logs = Activity::latest()->get();
 
-        return view('pages.logspage', compact('logs'));
+        return view('pages.logs-page', compact('logs'));
+    }
+
+    public function adminList()
+    {
+        $adminRole = Role::where('name', 'admin')->first();
+        $admins = User::role($adminRole)->get();
+
+        return view('pages.admin-list', compact('admins'));
     }
 }
