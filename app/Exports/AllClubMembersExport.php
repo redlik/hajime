cce2f2e354ab7511e998714ab7ce8abd8894617c
redlik <ralphr@outlook.com>
2024-07-24 06:54:01 +0100

    Inactive members export route and setup

--- app/Exports/AllClubMembersExport.php
+++ app/Exports/AllClubMembersExport.php
@@ -2,16 +2,84 @@
 
 namespace App\Exports;
 
-use App\Models\app\Models\Member;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use App\Models\Member;
+use App\Models\Grade;
+use App\Models\Membership;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
-class AllClubMembersExport implements FromCollection
+class AllClubMembersExport implements FromQuery, WithMapping, WithHeadings, WithTitle, ShouldAutoSize
 {
+    private int $club;
+
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function __construct(Int $club)
     {
-        return Member::all();
+
+        $this->club = $club;
+    }
+
+    public function query()
+    {
+
+        return Member::query()
+            ->where('club_id', $this->club)
+            ->orderBy('last_name')
+            ->orderBy('active', 'desc');
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Number',
+            'Name',
+            'Gender',
+            'Age',
+            'Club Name',
+            'Phone',
+            'Email',
+            'Active',
+            'Type',
+            'Join Date',
+            'Last Grade',
+            'Adaptive Judo',
+        ];
+    }
+
+    public function map($member): array {
+
+        $membership = Membership::where('member_id', $member->id)->latest('join_date')->first();
+        $grade = Grade::where('member_id', $member->id)->latest('grade_date')->first();
+        if (is_null($grade)) {
+            $grade = 'Not set';
+        } else
+            $grade = $member->latestGrade()->grade_level;
+
+        $adaptive = isset($member->adaptive) ? $member->special : 'No';
+
+        return [
+            $member->number,
+            $member->first_name." ".$member->last_name,
+            $member->gender,
+            $member->age,
+            $member->club->name,
+            $member->phone,
+            $member->email,
+            $member->active ? 'Active' : 'Not Active',
+            $membership->membership_type ?? 'None',
+            $membership->join_date ?? 'None',
+            $grade,
+            $adaptive,
+        ];
+    }
+
+    public function title(): string
+    {
+        return 'Club All Members List';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -96,6 +96,7 @@ Route::group(['middleware' => ['auth', 'role:admin'], 'prefix' => 'reports'], fu
 Route::group(['middleware' => ['auth', 'role:admin'], 'prefix' => 'exports'], function () {
     Route::get('/memberships/{start}/{end}', [MembershipReportController::class, 'export'])->name('report.membership.export');
     Route::get('/members/{club}/{start}/{end}', [MembersReportController::class, 'export'])->name('report.members.export');
+    Route::get('/all-members/{club}/', [MembersReportController::class, 'exportAll'])->name('report.members.export.all');
     Route::get('/clubs-status', [ClubStatusReportController::class, 'export'])->name('report.status.export');
     Route::get('/invalid-personnel', [InvalidPersonnelReportController::class, 'export'])->name('report.invalid.export');
     Route::get('/active-coaches', [ActiveCoachReportController::class, 'export'])->name('report.active.export');
