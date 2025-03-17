4ec5fd8e20c87524f6072c1546f777ef75c308c3
redlik <ralphr@outlook.com>
2025-03-17 15:08:18 +0000

    Activation email

---
+++ resources/views/emails/send-mobile-activation-codes.blade.php
@@ -0,0 +1,13 @@
+<x-mail::message>
+# Hello!
+
+This email has been used to activate your membership account on a Irish Judo mobile application.
+Please use the activation code below to complete your activation.
+
+# {{ $code }}
+
+The activation code is valid for 2 hours so kindly complete the process before the code expires.
+
+Thank you,<br>
+{{ config('app.name') }}
+</x-mail::message>
