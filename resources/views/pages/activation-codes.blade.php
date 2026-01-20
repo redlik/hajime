5423d51d35214985e680e6a9d0f2f5e81a851061
redlik <ralphr@outlook.com>
2026-01-20 10:23:11 +0000

    Change page title

--- resources/views/pages/activation-codes.blade.php
+++ resources/views/pages/activation-codes.blade.php
@@ -7,7 +7,7 @@
             <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
 
                 <header class="font-semibold text-xl bg-gray-600 text-gray-100 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
-                    Activity log
+                    Mobile app activation codes
                 </header>
                 <div class="fi-ta-container p-8 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-xl shadow-sm">
                     @livewire('activation-codes-view')
